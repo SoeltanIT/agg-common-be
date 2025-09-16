e7730033a121bfca5d0953447fe450a205251080
Achmad Rizky <atsi.achmadr@xlsmart.co.id>
2025-09-17 02:07:36 +0700

    feat: make pagination param public

--- pagination.go
+++ pagination.go
@@ -8,27 +8,27 @@ import (
 	"github.com/valyala/fasthttp"
 )
 
-// paginationParams : contains the pagination parameters.
-type paginationParams struct {
+// PaginationParams : contains the pagination parameters.
+type PaginationParams struct {
 	Page     int
 	PageSize int
 }
 
 // GetPage : returns the page number.
-func (p paginationParams) GetPage() int {
+func (p PaginationParams) GetPage() int {
 	return p.Page
 }
 
 // GetPageSize : returns the page size.
-func (p paginationParams) GetPageSize() int {
+func (p PaginationParams) GetPageSize() int {
 	return p.PageSize
 }
 
 // DefaultPageSize is the default number of items per page.
 const DefaultPageSize = 10
 
-// NewPaginationParams : creates a new paginationParams with default values.
-func NewPaginationParams(c *fiber.Ctx) paginationParams {
+// NewPaginationParams : creates a new PaginationParams with default values.
+func NewPaginationParams(c *fiber.Ctx) PaginationParams {
 	page := c.QueryInt("page", 1)
 
 	if page < 1 {
@@ -37,19 +37,19 @@ func NewPaginationParams(c *fiber.Ctx) paginationParams {
 
 	pageSize := c.QueryInt("pageSize", DefaultPageSize)
 
-	return paginationParams{
+	return PaginationParams{
 		Page:     page,
 		PageSize: pageSize,
 	}
 }
 
 // CalculateOffset : calculates the offset for the SQL query based on pagination parameters.
-func (p paginationParams) CalculateOffset() int {
+func (p PaginationParams) CalculateOffset() int {
 	return (p.Page - 1) * p.PageSize
 }
 
 // GetPaginationResponse : returns the pagination response.
-func (p paginationParams) GetPaginationResponse(req *fasthttp.Request, total int64) paginationResponse {
+func (p PaginationParams) GetPaginationResponse(req *fasthttp.Request, total int64) paginationResponse {
 	return paginationResponse{
 		Next:  p.GetNext(req.URI(), total),
 		Prev:  p.GetPrev(req.URI()),
@@ -59,7 +59,7 @@ func (p paginationParams) GetPaginationResponse(req *fasthttp.Request, total int
 }
 
 // GetPrev : returns the previous page URL.
-func (p paginationParams) GetPrev(baseURL *fasthttp.URI) string {
+func (p PaginationParams) GetPrev(baseURL *fasthttp.URI) string {
 	if p.Page <= 1 {
 		return ""
 	}
@@ -74,7 +74,7 @@ func (p paginationParams) GetPrev(baseURL *fasthttp.URI) string {
 }
 
 // GetNext : returns the next page URL.
-func (p paginationParams) GetNext(baseURL *fasthttp.URI, total int64) string {
+func (p PaginationParams) GetNext(baseURL *fasthttp.URI, total int64) string {
 	if total <= int64(p.Page*p.PageSize) {
 		return ""
 	}
